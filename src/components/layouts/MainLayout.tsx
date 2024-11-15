2c92023eb9ecb65a4d381a756e325e7f7965c12b
Logan Pritchett <logan.pritchett01@gmail.com>
2024-11-15 00:36:13 -0600

    style: create custom scrollbar

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -11,12 +11,14 @@ interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex min-h-screen items-center justify-center p-4 sm:p-8">
-      <GlassCard className="w-full min-h-[calc(100vh-2rem)] sm:min-h-[calc(100vh-4rem)] p-8 relative flex flex-col">
-        <div className="absolute top-8 right-8">
-          <ThemeToggle />
-        </div>
-        <div className="flex flex-1 gap-8">
+    <div className="fixed inset-0 p-4 sm:p-8 overflow-hidden">
+      {/* Updated ThemeToggle positioning */}
+      <div className="absolute top-4 right-4 sm:top-8 sm:right-8 z-50">
+        <ThemeToggle />
+      </div>
+
+      <GlassCard className="w-full h-full p-8 relative flex flex-col">
+        <div className="flex flex-1 gap-8 h-full">
           {/* Left Side - Static Content */}
           <div className="flex flex-col w-1/2 shrink-0 relative">
             <div className="flex flex-col h-full">
@@ -34,7 +36,7 @@ export function MainLayout({ children }: MainLayoutProps) {
           </div>
           
           {/* Right Side - Dynamic Content */}
-          <div className="flex-1 w-1/2 overflow-auto">
+          <div className="flex-1 w-1/2 overflow-y-auto custom-scrollbar">
             <PageTransition>
               {children}
             </PageTransition>
