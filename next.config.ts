287fb458b8b8fc4f4540f24aff403223ea680066
Logan Pritchett <logan.pritchett01@gmail.com>
2024-11-24 18:49:02 -0600

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
